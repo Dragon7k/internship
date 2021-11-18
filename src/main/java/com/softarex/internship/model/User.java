f3e5148889340b69e3c71d8381020d52ce259fae
Ivliev Vitaly <vitalik.ivliev.2013@mail.ru>
2021-11-18 16:22:33 +0300

    start

---
+++ src/main/java/com/softarex/internship/model/User.java
@@ -0,0 +1,24 @@
+package com.softarex.internship.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="usr")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private int id;
+    private String email;
+    private String password;
+    private String firstName;
+    private String secondName;
+    private String phoneNumber;
+
+}
